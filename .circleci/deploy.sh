2d38d6def990689358e9925c14fe9068db478efd
Vlad Shvaydetskiy <vshvaydetskiy@provectus.com>
2022-02-05 16:09:21 +0100

    Update

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -16,7 +16,7 @@ then
 fi
 
 payload=$(echo "{\"username\": \"$PORTAINER_LOGIN\",\"password\": \"$PORTAINER_PASSWORD\"}")
-json=$(curl --silent --location --request POST 'https://portainer.beta.envoys.vision/api/auth' --header 'Content-Type: application/json' --data-raw "$payload")
+json=$(curl --silent --location --request POST 'https://portainer.beta.envoys.vision/api/auth' -H 'Content-Type: application/json' -d "$payload")
 
 if [[ ${json} != *"jwt"* ]];then
       echo "Authorization failed. Exit."
@@ -27,4 +27,4 @@ echo "Authorization success. Start deploying version '$VERSION' to stack: $STACK
 TOKEN=$(echo $json | sed "s/{.*\"jwt\":\"\([^\"]*\).*}/\1/g")
 deploy=$(echo "{\"env\":[{\"name\":\"VERSION\",\"value\":\"$VERSION\"}],\"prune\":true,\"RepositoryReferenceName\":\"refs/heads/develop\",\"RepositoryAuthentication\":true,\"RepositoryUsername\":\"$REPOSITORY_LOGIN\",\"RepositoryPassword\":\"$REPOSITORY_PASSWORD\"}")
 curl --silent --location --request PUT "https://portainer.beta.envoys.vision/api/stacks/$STACK_ID/git/redeploy?endpointId=1" \
---header 'Content-Type: application/json' --header "Authorization: Bearer $TOKEN" --data-raw "$deploy"
+-H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -d "$deploy"
