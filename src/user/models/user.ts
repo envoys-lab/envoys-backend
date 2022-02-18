ef3330965a98c1075e05e3dc8e4fd3f7179b7f3f
Vlad Shvaydetskiy <vshvaydetskiy@codyua.com>
2022-02-18 20:54:28 +0100

    Fix lint

--- src/user/models/user.ts
+++ src/user/models/user.ts
@@ -4,7 +4,6 @@ import User from 'src/user/entity/user.entity'
 export const userWalletAddress = '0x484D3D5Bbc8114B794767FecbDeCf7887FDd04C2'
 export const verificationId = '17efc59c0018c2360e0b6e14f8a13c1c0fb8'
 export const applicantId = '92b57052056042486d0b81f47402cb0974ab'
-export const requestId = '17efc59c0018b2460f0b6e14f8a13e1c0cb8'
 export const userId = new ObjectID('6205840eae89880012c0ad47')
 
 export const userRecord = {
@@ -30,7 +29,7 @@ export const mockUserService = {
 }
 
 export const mockRepository = {
-  findOne: jest.fn((query) =>
+  findOne: jest.fn(() =>
     Promise.resolve({
       ...userRecord,
     }),
