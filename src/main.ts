717b4db0e126e666959cfcaf2e64eeaa82f842b2
Sergey <dev.zaycev@gmail.com>
2022-11-10 16:13:00 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
+import { ServerResponse } from 'http'
+import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -9,9 +11,16 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService)
   app.enableCors({
-    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
-    origin: '*'
+    origin: '*',
+    credentials: true
   });
+
+  app.use((_, res: Response, next: NextFunction) => {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH')
+    next();
+  });
+
+
   await app.listen(configService.get<number>('server.port'))
 }
 
